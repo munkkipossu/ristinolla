fb216a2dfe7597cbc261b3be9f2011911c9a8531
Teemu O Laakso <tolaakso@svm-26.cs.helsinki.fi>
2014-03-13 23:02:20 +0200

    Korvasin perus javaprojektin tuolla maven projektilla

---
+++ Ristinolla/src/main/java/harjoitustyo/ristinolla/App.java
@@ -0,0 +1,13 @@
+package harjoitustyo.ristinolla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
